5a162147bb685174fb911c50bcea1a6c4b903309
Carlos1704-C <carlosmenchacaortiz@gmail.com>
2022-11-16 19:50:03 -0600

    version 3.0 Se agrego el nodeJS para la conexion de base de datos Se agrego a la carpeta de Bootstrap responsivo contenido php y html

---
+++ Parcial3/configuracion.php
@@ -0,0 +1,11 @@
+<?php
+$registro["nombres"]= "Carlos Alberto";
+$registro["ApellidoMaterno"] = "Menchaca";
+$registro["ApellidoPaterno"] = "Ortiz";
+$registro["Correo"] = "carlosmenchacaortiz@gmail.com";
+$registro["FechaNacimiento"] = "16/11/2022";
+$registro["CURP"] = "MEOCO010417HTSNRRA5";
+$registro["Materia"] = "Taller de bases de datos";
+
+echo json_decode(registro);
+?>
--- ProyectoFinal/Pagina.js
+++ ProyectoFinal/Pagina.js
@@ -1,32 +1,13 @@
-const sql = require('mssql');
-//Configuracion de la base de datos
-var config = 
-{
-    user:'Carlos',
-    password:'carlos123',
-    database: 'dbPagina', //Nombre de la base de datos
-    server:'localhost',
-    options:
-    {
-        TrustedConnection: true
-    }
-};
-
-//Conexion a la base de datos
-
-sql.connect(config,function(err){
-    if(err){
-        console.log(err);
-    }
-    //Crear el objeto 
-    var request =  new sql.Request();
-    // Query consulta a la base de datos
-    request.query('SELECT * FROM Registros', function(err,recordSet){
-        if(err){
-            console.log(err);
-        }else{
-             console.log(recordSet);
-             console.log("Conectado existosamente");
-        }
-    });
+const config = require('./config');
+const rest = new (require('rest-mssql-nodejs'))
+({
+user: config.user,
+password: config.password,
+server: config.server,
+database: config.database,
 });
+
+setTimeout(async () => {
+    const resultado = await rest.executeQuery('SELECT * FROM dbPagina');
+    console.log(resultado.data);
+},1500);
