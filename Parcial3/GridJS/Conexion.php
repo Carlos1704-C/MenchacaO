ad190307e65539f0e717fa63d1aabf14fadc60c8
Carlos1704-C <carlosmenchacaortiz@gmail.com>
2022-11-29 14:51:18 -0600

    Se completo la tarea GridJS

---
+++ Parcial3/GridJS/Conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servidor = "localhost";
+$basededatos = "bdalumnos";
+$usuario = "admin";
+$password = "rEPB(3yt64_NwJ[V";
+$port = "3308";
+
+$con = mysqli_connect($servidor,$usuario,$password,$basededatos, $port) or die ("No se conecto");
+$consulta = "select * from alumnos";
+
+$registros = mysqli_query($con,$consulta) or die ("Problema en el select");
+
+$result = mysqli_fetch_all($registros);
+mysqli_close($con);
+echo json_encode($result);
+?>
